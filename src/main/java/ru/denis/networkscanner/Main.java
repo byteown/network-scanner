043a3e97ed05b453f40350b2aeebb6c1c088cddb
Denis Sorokin <r403prod@gmail.com>
2026-05-14 14:29:33 +0500

    feat: added greetings by name

--- src/main/java/ru/denis/networkscanner/Main.java
+++ src/main/java/ru/denis/networkscanner/Main.java
@@ -1,7 +1,16 @@
 package ru.denis.networkscanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите ваше имя: ");
+        String user_prompt = sc.nextLine();
+        if (user_prompt.isBlank()) {
+            System.out.println("Привет, незнакомец!");
+        } else {
+            System.out.println("Привет, " + user_prompt + "!");
+        }
     }
 }
